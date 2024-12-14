ae66c09594ff646daf7aac587536d41b60da5229
Amna Saleem <saleemamna431@gmail.com>
2024-12-13 22:59:48 -0700

    update

---
+++ WebApplication1/WebApplication1/Controllers/AddController1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AddController1 : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View(); 
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+    }
+}
