183ea74a538087abb0f5bfbb9dd2142f1330511b
Amna Saleem <saleemamna431@gmail.com>
2024-12-26 00:05:21 -0700

    api

---
+++ WebApplication4/WebApplication4/Controllers/HelloController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HelloController1 : Controller
+    {
+        public IActionResult Happy()
+        {
+            ViewBag.Message = "Message From View Bag";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+            string[] fruits = { "Apple", "Mango", "Banana", "Orange" };
+            ViewBag.FruitsArray = fruits;
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hockey",
+            };
+            return View();
+        }
+    }
+}
